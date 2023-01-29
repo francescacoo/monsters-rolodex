53481d4bfb9c20d71278288c3db15d7140f7a8c5
Francesca <francescacoo@gmail.com>
2023-01-29 19:47:57 +0100

    lesson37

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 import { Component } from 'react';
 
-
-
 class App extends Component {
-
-constructor(){
-  super();
-  this.state={
-    monsters:[
-      {
-        name: "Linda"
-      },
-      {
-        name:"Frank"
-      },
-      {
-        name: "Jackie"
-      },
-      {
-        name: "Andrei"
-      }
-    ]
-   
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
   }
-}
 
-  render(){
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then((users)=> this.setState(() => {
+      return {monsters: users}
+
+
+    }))
+  }
+
+
+  render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
-            return <h1>{monster.name}</h1>
-          })
-        }
-
+        {this.state.monsters.map((monster) => {
+          return (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+            </div>
+            )
+            ;
+        })}
       </div>
     );
   }
- 
 }
 
 export default App;
