17ebad919bd7eaa2ce376ab46d9d77aa4eb29f7f
Francesca <francescacoo@gmail.com>
2023-02-02 22:35:47 +0100

    43

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField:''
     };
   }
 
@@ -13,16 +14,33 @@ class App extends Component {
     .then(response => response.json())
     .then((users)=> this.setState(() => {
       return {monsters: users}
-
-
     }))
   }
 
 
   render() {
+    const filteredMonsters=this.state.monsters.filter( (monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={(event)=> {
+            console.log(event.target.value);
+            const searchField = event.target.value.toLocaleLowerCase();
+         
+            this.setState(
+              () => {
+                return { searchField}
+            })
+          }} 
+        />
+        
+        {filteredMonsters.map((monster) => {
           return (
           <div key={monster.id}>
             <h1>{monster.name}</h1>
