cab3e4c72365e1c26d6db20d7aaa679825dada96
Francesca <francescacoo@gmail.com>
2023-02-04 11:16:01 +0100

    lesson 49

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
-
+import SearchBox from './components/search-box/search-box.component';
+import CardList from './components/card-list/card-list.component';
 class App extends Component {
   constructor() {
     super();
@@ -17,37 +18,28 @@ class App extends Component {
     }))
   }
 
+  onSearchChange = (event)=> {
+    const searchField = event.target.value.toLocaleLowerCase(); 
+    this.setState(
+      () => {
+        return { searchField}
+    })
+  }
+
 
   render() {
-    const filteredMonsters=this.state.monsters.filter( (monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+
+    const { monsters, searchField}=this.state;
+    const {onSearchChange}=this;
+    const filteredMonsters=monsters.filter( (monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
       <div className="App">
-        <input 
-          className='search-box' 
-          type='search' 
-          placeholder='search monsters' 
-          onChange={(event)=> {
-            console.log(event.target.value);
-            const searchField = event.target.value.toLocaleLowerCase();
-         
-            this.setState(
-              () => {
-                return { searchField}
-            })
-          }} 
-        />
-        
-        {filteredMonsters.map((monster) => {
-          return (
-          <div key={monster.id}>
-            <h1>{monster.name}</h1>
-            </div>
-            )
-            ;
-        })}
+        <SearchBox onChangeHandler={onSearchChange} placeholder={'search monsters'} className='search-box'/>
+        <CardList monsters= {filteredMonsters} />
+     
       </div>
     );
   }
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+class SearchBox extends Component {
+    render(){
+        return(
+
+            <input 
+          //className='search-box' 
+          type={this.props.className}
+          placeholder={this.props.placeholder}
+          onChange={this.props.onChangeHandler} 
+        />
+
+        )
+    }
+
+}
+
+export default SearchBox;
